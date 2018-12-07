821a213a97a5f8f7f4e5dd2b8404d3a2de2cf2aa
Hélio Marcondes <helio@prodweb.com.br>
2018-12-07 12:11:36 -0200

    Fix vanilla script + example

--- helios.customSelect.vanilla.js
+++ helios.customSelect.vanilla.js
@@ -1,99 +1,95 @@
-var heliosCustomSelect = function (el, options) {
-  this.el = document.querySelector(el);
-  this.options = options || {};
+// Create an immediately invoked functional expression to wrap our code
+(function() {
+  //Construtor
+  this.heliosCustomSelect = function(el, options) {
+    // Create global element references
+    this.element = el;
+    this.defaults = options;
+    var elementDOM = document.querySelector(el);
 
-  for (var optionName in this.defaultOptions) {
-    if (typeof this.options[optionName] === 'undefined') {
-      this.options[optionName] = this.defaultOptions[optionName];
-    }
-  }
-}
+    // Define option defaults
+    var defaults = {
+      list: ["Option 1", "Option 2", "Option 3"],
+      defaultOption: "Choose an option"
+    };
 
-heliosCustomSelect.prototype.defaultOptions = {
-  list: ['Option 1', 'Option 2', 'Option 3'],
-  defaultOption: 'Choose an option'
-}
+    //Private Methods
+    function buildSelect() {
+      var selfElement = el;
+      var self = this;
 
-heliosCustomSelect.prototype.mount = function () {
-
-  var self = this;
-
-  /*Cria o DOM da lista*/
-  let frag = document.createDocumentFragment();
-
-  let fragWrapper = frag.appendChild(document.createElement("div"));
-  fragWrapper.className = "wrapper-select";
-
-  let fragSelectAtual = fragWrapper.appendChild(
-    document.createElement("div")
-  );
-  fragSelectAtual.className = "select__atual";
-
-  if (typeof this.options.defaultOption !== 'undefined') {
-
-    let selectAtualText = fragSelectAtual.appendChild(document.createElement("span"));
-
-    selectAtualText.className = "select__atual__text";
-    selectAtualText.textContent = this.options.defaultOption;
-  }
-
-  let selectIcon = fragSelectAtual.appendChild(
-    document.createElement("span")
-  );
-
-  selectIcon.className = "select__icon";
-
-  let selectOpcoes = fragWrapper.appendChild(document.createElement("div"));
-
-  selectOpcoes.className = "select__opcoes";
-
-  fragSelectAtual.onclick = function () {
-    fragSelectAtual.classList.toggle("open");
-    selectOpcoes.classList.toggle("open");
-  };
-
-  if (typeof this.options.list !== 'undefined') {
-
-    let items = this.options.list;
-
-    for (let index = 0; index < items.length; index++) {
-      const element = items[index];
-      let radioLabel = selectOpcoes.appendChild(document.createElement("label"));
-      radioLabel.className = "select__opcoes__label";
-      radioLabel.setAttribute("tabindex", index + 1);
-      radioLabel.setAttribute("for", element);
-      radioLabel.textContent = element;
-
-
-
-      radioLabel.onfocus = function (el) {
-        document.addEventListener("keydown", function (e) {
-          if (e.keyCode === 13) {
-
-          }
-        });
-      }
-
-      radioLabel.onclick = function () {
-        let selectText = this.textContent;
-        let selectAtualText = document.querySelector('.select__atual__text');
-        selectAtualText.textContent = self.options.defaultOption + ' ' + selectText;
-        document.querySelector('.select__atual').classList.remove('open');
-        document.querySelector('.select__opcoes').classList.remove('open');
-      };
-
-
-
-      let inputRadio = fragWrapper.insertBefore(
-        document.createElement("input"),
-        fragSelectAtual
+      /*Cria o DOM da lista*/
+      let frag = document.createDocumentFragment();
+      let fragWrapper = frag.appendChild(document.createElement("div"));
+      fragWrapper.className = "wrapper-select";
+      let fragSelectAtual = fragWrapper.appendChild(
+        document.createElement("div")
       );
 
-      inputRadio.setAttribute("type", "radio");
-      inputRadio.setAttribute("id", element);
-      inputRadio.setAttribute("name", "optSelect");
-    }
-  }
+      fragSelectAtual.className = "select__atual";
+      if (typeof options !== "undefined") {
+        let selectAtualText = fragSelectAtual.appendChild(
+          document.createElement("span")
+        );
+        selectAtualText.className = "select__atual__text";
+        selectAtualText.textContent = options.defaultOption;
+      } else {
+        let selectAtualText = fragSelectAtual.appendChild(
+          document.createElement("span")
+        );
+        selectAtualText.className = "select__atual__text";
+        selectAtualText.textContent = defaults.defaultOption;
+      }
+      let selectIcon = fragSelectAtual.appendChild(
+        document.createElement("span")
+      );
 
-  this.el.appendChild(frag);
-}
+      selectIcon.className = "select__icon";
+      let selectOpcoes = fragWrapper.appendChild(document.createElement("div"));
+      selectOpcoes.className = "select__opcoes";
+      fragSelectAtual.onclick = function() {
+        fragSelectAtual.classList.toggle("open");
+        selectOpcoes.classList.toggle("open");
+      };
+
+      if (typeof options !== "undefined") {
+        var items = options.list;
+      } else {
+        var items = defaults.list;
+      }
+      for (let index = 0; index < items.length; index++) {
+        const element = items[index];
+        let radioLabel = selectOpcoes.appendChild(
+          document.createElement("label")
+        );
+        radioLabel.className = "select__opcoes__label";
+        radioLabel.setAttribute("tabindex", index + 1);
+        radioLabel.setAttribute("for", element);
+        radioLabel.textContent = element;
+        radioLabel.onfocus = function(el) {
+          document.addEventListener("keydown", function(e) {
+            if (e.keyCode === 13) {
+            }
+          });
+        };
+        radioLabel.onclick = function() {
+          let selectText = this.textContent;
+          let selectAtual = this.parentNode.previousSibling.children[0];
+          selectAtual.textContent = selectText;
+          selectAtual.parentNode.classList.remove("open");
+          selectAtual.parentNode.nextSibling.classList.remove("open");
+        };
+        let inputRadio = fragWrapper.insertBefore(
+          document.createElement("input"),
+          fragSelectAtual
+        );
+        inputRadio.setAttribute("type", "radio");
+        inputRadio.setAttribute("id", element);
+        inputRadio.setAttribute("name", "optSelect" + selfElement);
+      }
+
+      elementDOM.appendChild(frag);
+    }
+    return buildSelect();
+  };
+})();
